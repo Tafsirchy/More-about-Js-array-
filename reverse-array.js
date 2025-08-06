9754af7ea029156182911cafc6a676bcfff765a9
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-06 22:04:24 +0600

    reverse-array

---
+++ reverse-array.js
@@ -0,0 +1,25 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//reverse = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+const reversed = [];
+
+// for(let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i]);
+//     reversed.unshift(numbers[i]);
+// }
+
+// for (let i = numbers.length-1; i>=0; i--){
+//     reversed.push(numbers[i]);
+// }
+
+// for(const num of numbers){
+//     console.log(num);
+//     reversed.unshift(num);
+// }
+
+console.log(numbers);
+numbers.reverse();
+console.log(numbers);
+
+// console.log(reversed);
+
