82b6246c0ef2b12ea4ef51c227690256274f4f27
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-06 20:51:55 +0600

    array-looping

---
+++ array-looping.js
@@ -0,0 +1,28 @@
+const fruits = ['apple', 'banana', 'mango', 'orange', 'jackfruit','papaya', 'pineapple'];
+// console.log(fruits[0]);
+
+// for loop
+for (let i = 0; i < fruits.length; i++) {
+    // console.log(i);
+    // console.log(fruits[i]);
+}
+
+const number = [12, 45, 78, 145, 7, 44, 765]
+
+for(let i = 0; i < number.length; i++){
+    // console.log(number[i]);
+}
+
+let i = 0;
+while(i < fruits.length){
+    console.log(fruits[i]);
+    i++;
+}
+
+const prices = [15, 80, 49, 78, 26];
+
+let j = 0;
+while(j < prices.length){
+    console.log(prices[j]);
+    j++;
+}
