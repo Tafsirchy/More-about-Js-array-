742923ea6d3ccef0fbbf2544bfa0b6cfbaf398ac
Tafsirchy <tafsirchy1000@gmail.com>
2025-08-07 20:18:52 +0600

    task6

---
+++ task6.js
@@ -0,0 +1,13 @@
+const student = [
+  { name: "John", marks: 85 },
+  { name: "Alice", marks: 90 }
+];
+
+console.log(student[0].name + ' scored ' + student[0].marks);
+console.log(student[1].name + ' scored ' + student[1].marks);
+
+// johnsMarks = student[0].name +' scored ' + student[0].marks;
+// console.log(johnsMarks);
+
+// alicesMarks = student[1].name + ' scored ' + student[1].marks;
+// console.log(alicesMarks);
